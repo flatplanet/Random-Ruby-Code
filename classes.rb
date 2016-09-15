bcdfdabc1e00b4c7faa7c1f647c31d8d8c80d9fe
John Elder <johne4196@gmail.com>
2016-09-15 13:47:38 +0000

    classes.rb Intro To Classes

--- classes.rb
+++ classes.rb
@@ -1,25 +1,27 @@
 ################################################################
 #
 # Basic Class
+# Create a Class, Initialize it, Call it (passing in some data)
+# Return a Result
 #
 ################################################################
 
 
 class Product
-		def initialize( description, price)
-		    # generate a random number for the ID just for fun
-    		@id = rand(100...999)
-    		
-    		# suck in our description and price from the outside
-    		@description = description
-    		@price = price
-    		
-		end
-
-		def to_s
-		    # return by rewriting to_s :-p
-		    return "#{@id}\t#{@description}\t#{@price}"
-		end
+    def initialize( description, price)
+        # generate a random number for the ID just for fun
+    	@id = rand(100...999)
+    	
+    	# suck in our description and price from the outside
+    	@description = description
+    	@price = price
+    	
+    end
+    
+    def to_s
+        # return by rewriting to_s :-p
+        return "#{@id}\t#{@description}\t#{@price}"
+    end
 end
 
 book = Product.new( "Ruby On Rails For Web Development", 26.95)
