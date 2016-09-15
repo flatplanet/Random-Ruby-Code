27d7b3b30c100b109e8dcfdb47052e86d27bc3e5
John Elder <johne4196@gmail.com>
2016-09-15 13:46:11 +0000

    classes.rb intro to Classes

---
+++ classes.rb
@@ -0,0 +1,29 @@
+################################################################
+#
+# Basic Class
+#
+################################################################
+
+
+class Product
+		def initialize( description, price)
+		    # generate a random number for the ID just for fun
+    		@id = rand(100...999)
+    		
+    		# suck in our description and price from the outside
+    		@description = description
+    		@price = price
+    		
+		end
+
+		def to_s
+		    # return by rewriting to_s :-p
+		    return "#{@id}\t#{@description}\t#{@price}"
+		end
+end
+
+book = Product.new( "Ruby On Rails For Web Development", 26.95)
+book2 = Product.new( "Intro To Ruby", 25.95)
+
+puts book
+puts book2
