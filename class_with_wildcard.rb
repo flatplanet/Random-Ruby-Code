3fae0b0d87d77a476e498e0eeab4e156fe5fca3b
John Elder <johne4196@gmail.com>
2016-09-19 14:37:13 +0000

    class with wildcard splat arguments

--- class_with_wildcard.rb
+++ class_with_wildcard.rb
@@ -4,7 +4,13 @@ class Product
   def initialize( *placeholder_args )
     #suck in whatever you passed during initialization into a @whatever array
     @whatever = placeholder_args
+    @length2 = @whatever.length
   end
+  
+  def do_something
+      @do_something = @whatever
+  end
+  
 end
 
 # Set it up...
@@ -12,5 +18,5 @@ book = Product.new( "a thing", "another thing", 3, 4, "last thing")
 
 
 # Call the thing!
-puts book.inspect
+puts book.do_something[2]
 
