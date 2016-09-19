551abd367897f97653ef7291aa7a9222ba38b052
John Elder <johne4196@gmail.com>
2016-09-19 17:49:52 +0000

    Class Inheritance

---
+++ class_inheritance.rb
@@ -0,0 +1,21 @@
+class Animal
+	attr_accessor :color, :name
+	def initialize ( name, color )
+		@name = name
+		@color = color
+	end
+end
+
+class Lion < Animal
+	def speak
+		return "RAWR!"
+	end
+end
+
+lion = Lion.new( "lion", "golden" )
+
+# Inspect
+puts lion.inspect
+
+# Speak
+puts lion.speak
