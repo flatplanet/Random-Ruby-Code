71cbd834d1bbab4104386027cbe61abdb991efa5
John Elder <johne4196@gmail.com>
2016-09-20 20:25:19 +0000

    Double Arrays for Word Search Puzzle

---
+++ double_arrays.rb
@@ -0,0 +1,14 @@
+# Use double arrays to create a grid for a word search puzzle
+
+box = []
+
+10.times do | row |
+  box[row] = []
+  10.times do
+    box[row] << "*"
+  end
+end
+
+box.each do | row |
+  puts row.join( ' ' )
+end
